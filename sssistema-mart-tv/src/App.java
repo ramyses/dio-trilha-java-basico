f9cd40830815224b46fa99b07b9c7e834e7cba2d
ramyses <lukasramysesleao@gmail.com>
2024-05-14 12:38:53 -0400

    create class SmartTv

--- sssistema-mart-tv/src/App.java
+++ sssistema-mart-tv/src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV Ligada " +  smartTv.ligada);
+        System.out.println("Canal Atual : " +  smartTv.canal);
+        System.out.println("Volume Atual : " +  smartTv.volume);
+
+        
     }
 }
