885032eccbc34ac40a0af85c490c156d7aca0436
ramyses <lukasramysesleao@gmail.com>
2024-05-14 12:54:21 -0400

    finalizando aulas de métodos

--- sssistema-mart-tv/src/App.java
+++ sssistema-mart-tv/src/App.java
@@ -6,6 +6,24 @@ public class App {
         System.out.println("Canal Atual : " +  smartTv.canal);
         System.out.println("Volume Atual : " +  smartTv.volume);
 
+        smartTv.ligar ();
+        System.out.println("Novo status: TV Ligada : " +  smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo status: TV Ligada : " +  smartTv.ligada);
+
+
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
         
+
+        smartTv.reduzirVolume();
+        
+
+
+
+        smartTv.mudarCanal(13);
+        System.out.println("Canal Atual : " + smartTv.canal);
     }
 }
--- sssistema-mart-tv/src/SmartTv.java
+++ sssistema-mart-tv/src/SmartTv.java
@@ -3,5 +3,29 @@ public class SmartTv {
     int canal = 1;
     int volume = 25;
 
-    
+    public void ligar(){
+        ligada = true;
+    }
+    public void desligar(){
+        ligada = false;
+    }
+
+    public void mudarCanal(int novoCanal) {
+        canal = novoCanal;
+    }
+    public void aumentarCanal() {
+        canal++;
+    }
+    public void reduzirCanal() {
+        canal--;
+    }
+
+    public void aumentarVolume(){
+        volume ++;
+        System.out.println("Volume: " + volume);
+    }
+    public void reduzirVolume(){
+        volume --;
+        System.out.println("Volume: " + volume);
+    }
 }
