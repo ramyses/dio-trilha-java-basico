2cebe6f04f54ece4362b2e212d213ff44cff007f
ramyses <lukasramysesleao@gmail.com>
2024-05-14 12:33:09 -0400

    create sitema mart tv

--- TIPOS-VARIAVEIS/src/TiposVariaveis.java
+++ TIPOS-VARIAVEIS/src/TiposVariaveis.java
@@ -1,5 +1,30 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+        
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+
+        //int numero = 1;
+
+        //numero = 2;
+        //System.out.print(numero);
+        final double VALOR_DE_PI = 3.34;
+
+        //tipos primitivos
+        //estudar a classe String que reppresenta texto na aplicacao
+
+        String meuNome = "lukas ramyses";
+
+
+        int numero = 5;
+        numero =  - numero;
+        System.out.println(numero);
+
+        numero = - numero;
+        System.out.println(numero);
+
     }
 }
