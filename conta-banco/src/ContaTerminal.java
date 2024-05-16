103086ad45df7ba57a70db558d0d782a3aca73ba
ramyses <lukasramysesleao@gmail.com>
2024-05-16 11:49:20 -0400

    ajustes iniciais no projeto conta-banco

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,8 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        //TODO: Conhecer e importar a classe Scanner
+        //Exibir as mensagens para o nosso usuário
+        //Obter pela scanner os valores digitados no terminal
+        //Exibir a mensagem conta criada
+    }
+}
