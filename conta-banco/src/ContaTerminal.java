8ebd5d6cc85ae753576d217515362bbdec9ff4b2
ramyses <lukasramysesleao@gmail.com>
2024-05-16 23:45:55 -0400

    correcao texto resultado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         //TODO: Conhecer e importar a classe Scanner
+        //Exibir as mensagens para o nosso usuário
+        //Obter pela scanner os valores digitados no terminal
+        //Exibir a mensagem conta criada
+        
         Scanner scanner = new Scanner(System.in);
     
 
@@ -22,15 +26,9 @@ public class ContaTerminal {
         double saldo = scanner.nextDouble();
         
 
-        System.out.println("Olá " +nomeCliente+", obrigado p0or criar uma conta em nosso banco, sua agência é "+agencia+", conta "+ numero +" e seu saldo "+saldo+" já está disponível para saque");
+        System.out.println("Olá " +nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+ numero +" e seu saldo "+saldo+" já está disponível para saque");
 
-        //Exibir as mensagens para o nosso usuário
-
-
-        //Obter pela scanner os valores digitados no terminal
-
-
-        //Exibir a mensagem conta criada
+        
 
 
     }
